413bb21a91eee3e28c23073376606bb68f585a9e
Tabb7 <arthurvenceguerratabbi@aluno.itj.g12.br>
2026-06-25 11:49:43 -0300

    Update conexao.java

--- entidades/conexao.java
+++ entidades/conexao.java
@@ -1,17 +1,22 @@
-package stockmanager_;
+package stockmanager_database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager; 
+import java.sql.SQLException;
 
 public class conexao {
-    public static Connection conectar() throws Exception {
-        Class.forName("org.postgresql.Driver");
-
-        // Verifique se a sua senha do banco ainda é 1234 ou 123
+    
+   
+    public static Connection conectar() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver PostgreSQL não encontrado no projeto!", e);
+        }
+        
         String url = "jdbc:postgresql://localhost:5432/banco_loja_rafael_e_arthur";
         String user = "postgres";
         String password = "1234"; 
-
         return DriverManager.getConnection(url, user, password);
     }
 }
