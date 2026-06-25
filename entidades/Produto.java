cfbbeb907e909bf696cd2fb12b13fcc5301a4f8c
Tabb7 <arthurvenceguerratabbi@aluno.itj.g12.br>
2026-06-25 11:49:00 -0300

    Update Produto.java

--- entidades/Produto.java
+++ entidades/Produto.java
@@ -1,28 +1,30 @@
-package stockmanager_;
+package stockmanager_model;
 
 public class Produto {
-
+    
+    private int id; 
     private String nome;
     private int quantidade;
     private double preco;
 
-    public Produto(String nome, int quantidade, double preco) {
-        this.nome = nome;
-        this.quantidade = quantidade;
-        this.preco = preco;
+   
+    public int getId() {
+        return id;
     }
 
-    public String getNome() {
-        return nome;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
+    
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+    
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
 }
 
 
